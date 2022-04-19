dc0b5a7966dd844909f396ff7a0e95184af42e95
dingxp <dingxp@7000mall.com>
2022-04-19 17:28:46 +0800

    关于我页面修改

--- src/pages/AboutMe.vue
+++ src/pages/AboutMe.vue
@@ -1,27 +1,128 @@
 <template>
   <div class="about_me">
-    <AboutMeMain />
+    <div class="left">
+      <div v-for="item in data" :key="item.url" @click="toPage(item.id)">
+        <component
+          :is="item.component"
+          :class="item.id === activeId ? 'active' : ''"
+        ></component>
+      </div>
+    </div>
+    <div class="main">
+      <component :is="showComponent"></component>
+    </div>
     <div class="backhome" @click="toHome">
       <backward-filled />
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
-import AboutMeMain from "@/components/aboutme/AboutMeMain.vue";
-import { BackwardFilled } from '@ant-design/icons-vue';
-import useLayout from '@/store/layout';
-const router = useRouter();
+import { ref, shallowRef } from "vue";
+import useLayout from "@/store/layout";
+import {
+  HomeFilled,
+  IdcardFilled,
+  BookFilled,
+  AppleFilled,
+  ReadFilled,
+  ContactsFilled,
+  BackwardFilled,
+} from "@ant-design/icons-vue";
+import Home from "@/pages/aboutme/Home.vue";
+import Me from "@/pages/aboutme/Me.vue";
+import Work from "@/pages/aboutme/Work.vue";
+import Project from "@/pages/aboutme/Project.vue";
+import Web from "@/pages/aboutme/Web.vue";
+import Contact from "@/pages/aboutme/Contact.vue";
+const data = [
+  {
+    id: 0,
+    showComponent: Home,
+    component: HomeFilled,
+  },
+  {
+    id: 1,
+    showComponent: Me,
+    component: IdcardFilled,
+  },
+  {
+    id: 2,
+    showComponent: Work,
+    component: BookFilled,
+  },
+  {
+    id: 3,
+    showComponent: Project,
+    component: AppleFilled,
+  },
+  {
+    id: 4,
+    showComponent: Web,
+    component: ReadFilled,
+  },
+  {
+    id: 5,
+    showComponent: Contact,
+    component: ContactsFilled,
+  },
+];
+const activeId = ref(0);
+const showComponent = shallowRef(Home);
+const flag = ref(false);
 const layout = useLayout();
-const toHome = () =>{
-  layout.showAboutMe = false
-}
+const toHome = () => {
+  window.removeEventListener("mousewheel", mousewheel);
+  layout.showAboutMe = false;
+};
+const toPage = (id: number) => {
+  activeId.value = id;
+  showComponent.value = data[id].showComponent;
+};
+const mousewheel = (e: WheelEventInit) => {
+  if (flag.value) {
+    return;
+  } else {
+    flag.value = true;
+    if (e.deltaY && e.deltaY > 0) {
+      activeId.value++
+      if(activeId.value > data.length - 1){
+        activeId.value = data.length - 1;
+        showComponent.value = data[activeId.value].showComponent;
+      }else{
+        showComponent.value = data[activeId.value].showComponent;
+      }
+    }else if(e.deltaY && e.deltaY < 0){
+      activeId.value--
+      if(activeId.value < 0){
+        activeId.value = 0;
+        showComponent.value = data[activeId.value].showComponent;
+      }else{
+        showComponent.value = data[activeId.value].showComponent;
+      }
+    }
+    setTimeout(() => {
+      flag.value = false;
+    }, 100);
+  }
+};
+window.addEventListener("mousewheel", mousewheel);
 </script>
 
 <style scoped lang="less">
 .about_me {
   width: 100vw;
   height: 100vh;
+  position: relative;
+  display: flex;
+  .left {
+    width: 80px;
+    height: 100%;
+    border-right: 1px solid #f5f5f5;
+    padding-top: 30px;
+  }
+  .main {
+    flex: 1;
+  }
   .backhome {
     width: 50px;
     height: 50px;
@@ -32,24 +133,44 @@ const toHome = () =>{
     bottom: 0;
     margin: auto 0;
     cursor: pointer;
-    animation: back .5s ease-in-out infinite;
-    :deep(.anticon){
+    animation: back 0.5s ease-in-out infinite;
+    :deep(.anticon) {
       width: 50px;
       margin: 0 auto;
       line-height: 50px;
       font-size: 26px;
+      background: transparent;
+      color: #333;
     }
   }
-}
-@keyframes back {
-  0% {
-    transform: translateX(-5px);
+  :deep(.anticon) {
+    display: inline-block;
+    margin-left: 15px;
+    width: 50px;
+    height: 50px;
+    text-align: center;
+    line-height: 50px;
+    background: #ebebeb;
+    color: #67686d;
+    border-radius: 50%;
+    cursor: pointer;
+    margin-bottom: 15px;
+    font-size: 20px;
   }
-  50% {
-    transform: translateX(0);
+  .active {
+    background: #383334;
+    color: #eeecea;
   }
-  100% {
-    transform: translateX(5px);
+  @keyframes back {
+    0% {
+      transform: translateX(-5px);
+    }
+    50% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(5px);
+    }
   }
 }
 </style>
--- src/pages/aboutme/Home.vue
+++ src/pages/aboutme/Home.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="home">
-    <h1>一只小白u</h1>
-    <p>I AM A <span>Web Developer</span></p>
+    <div class="center">
+      <h1>一只小白u</h1>
+      <p>I AM A <span>Web Developer</span></p>
+    </div>
   </div>
 </template>
 <script setup lang="ts"></script>
 
 <style scoped lang="less">
 .home {
-  flex: 1;
+  width: 100%;
+  height: 100%;
   position: relative;
   background: #f2f3f5;
-  text-align: center;
-  overflow-y: scroll;
   .center {
     position: absolute;
     top: 50%;
     left: 0;
     right: 0;
     transform: translateY(-50%);
+    text-align: center;
     h1 {
       font-size: 100px;
       line-height: 140px;
